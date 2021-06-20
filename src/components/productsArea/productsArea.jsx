af5642c77eb6c351d800686e5c99531ef7fb184f
re7za <syd.id7@gmail.com>
2021-06-20 18:25:17 +0430

    Add Products grid area

---
+++ src/components/productsArea/productsArea.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import { ProductsGrid } from "../productsGrid";
+
+export const ProductsArea = () => {
+  return (
+    <div>
+      <ProductsGrid />
+    </div>
+  );
+};
---
+++ src/components/productsGrid/productsGrid.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+import { ProductCard } from "../../lib/productCard";
+
+import { products } from "../../services/mock";
+import style from "../../assets/styles/components/productsGrid/style.module.scss";
+
+export const ProductsGrid = () => {
+  return (
+    <div className={style.grid}>
+      {products.map((product) => (
+        <div key={product.id}>
+          <ProductCard {...product} />
+        </div>
+      ))}
+    </div>
+  );
+};
