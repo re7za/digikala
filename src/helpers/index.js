cec0c4733f9aa1dfa6656e95fb3988cf5818d4a7
re7za <syd.id7@gmail.com>
2021-06-20 18:25:21 +0430

    Add & Initial pages and their styles

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,5 +1,3 @@
-export * from "./auth-header";
-export * from "./handle-response";
 export * from "./http-client";
 export * from "./history";
 export * from "./role";
---
+++ src/pages/products/products.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+import { ProductsArea } from "../../components/productsArea";
+
+import style from "../../assets/styles/pages/products/style.module.scss";
+
+export const Products = () => {
+  return (
+    <div className={style.container}>
+      <aside className={style.aside}>aside</aside>
+      <section className={style.section}>
+        <ProductsArea />
+      </section>
+    </div>
+  );
+};
