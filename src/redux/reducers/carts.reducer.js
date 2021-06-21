72998645f427ac43d655d7f08d092ba4cf44f5a3
re7za <syd.id7@gmail.com>
2021-06-21 14:54:02 +0430

    Store cartsIds in localStorage

--- src/redux/reducers/carts.reducer.js
+++ src/redux/reducers/carts.reducer.js
@@ -1,22 +1,30 @@
 import { cartsConstants } from "../../constants/carts.constants.js";
 
 const initialState = {
-  cartsIds: [],
+  cartsIds: JSON.parse(localStorage.getItem("cartsIds")) ?? [],
 };
 
 export const cartsReducer = (state = initialState, action) => {
+  let updatedList = [];
   switch (action.type) {
     case cartsConstants.ADD_PRODUCT_TO_CARTS:
+      updatedList = state.cartsIds.includes(action.payload.data)
+        ? state.cartsIds
+        : [...state.cartsIds, action.payload.data];
+      localStorage.setItem("cartsIds", JSON.stringify(updatedList));
       return Object.assign({}, state, {
-        cartsIds: !state.cartsIds.includes(action.payload.data)
-          ? [...state.cartsIds, action.payload.data]
-          : state.cartsIds,
+        cartsIds: updatedList,
       });
+
     case cartsConstants.REMOVE_PRODUCT_FROM_CARTS:
+      updatedList = state.cartsIds.filter((id) => id !== action.payload.data);
+      localStorage.setItem("cartsIds", JSON.stringify(updatedList));
       return Object.assign({}, state, {
-        cartsIds: state.cartsIds.filter((id) => id !== action.payload.data),
+        cartsIds: updatedList,
       });
+
     case cartsConstants.REMOVE_ALL_CARTS:
+      localStorage.removeItem("cartsIds");
       return Object.assign({}, state, {
         cartsIds: [],
       });
